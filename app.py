89a65f5851a996c663cc12beb69f0071980d2f57
Kazuki Ohta <kazuki.ohta.kazu@gmail.com>
2022-10-20 14:53:17 +0900

    ログイン機能の実装

--- app.py
+++ app.py
@@ -1,15 +1,22 @@
-from flask import Flask, request, redirect, render_template 
+from flask import Flask, request, redirect, render_template, session
 from models import dbConnect
+from datetime import timedelta
+import secrets
 import uuid
 
 
 app = Flask(__name__)
+# セッション情報（クライアントのCookieに保存される）を暗号化する秘密鍵の生成
+app.secret_key = secrets.token_hex()
+# セッションの寿命を設定
+app.permanent_session_lifetime = timedelta(days=30)
 
 
 # サインアップページにページ遷移
 @app.route('/signup')
 def signup():
     return render_template('registration/signup.html')
+
 
 # サインアップ機能
 @app.route('/signup', methods=['POST'])
@@ -21,11 +28,37 @@ def userSignup():
     password2 = request.form.get('password2')
 
     # ユーザーidの生成
-    uid = uuid.uuid4()
+    uid = str(uuid.uuid4())
 
     # データベースの操作
     dbConnect.createUser(uid, name, email, password1)
+    # セッションの確立
+    session['uid'] = uid
+
     return redirect('/')
+
+
+# ログインページにページ遷移
+@app.route('/login')
+def login():
+    return render_template('registration/login.html')
+
+
+# ログイン機能
+@app.route('/login', methods=['POST'])
+def userLogin():
+    # フォームで入力された情報の取得
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    # データベースからユーザーを取得する
+    user = dbConnect.getUser(email)
+
+    if user != None and password == user['password']:
+        # セッションの確立
+        session['uid'] = user['uid']
+
+        return redirect('/')
 
 
 @app.route('/')
