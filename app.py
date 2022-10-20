1d4e4bc80f6385fb7a4d8413fe52584c55527db1
Kazuki Ohta <kazuki.ohta.kazu@gmail.com>
2022-10-20 11:01:08 +0900

    修正

--- app.py
+++ app.py
@@ -1,16 +1,13 @@
 from flask import Flask
 
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return 'Hello World'
 
-#チャネルの作成
 
-
-if __name__ == '__main__':
-    app.debug = True
-    app.run(host='0.0.0.0', port=8000)
-
-    # piyopiyo
+if __name__ == "__main__":
+    app.run(port=8080, debug=True)
