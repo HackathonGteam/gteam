37c2f18c9bade7af797d5561cc9c1ed86304d0e2
hashiguchi <shguppy3510@gmail.com>
2022-11-23 14:36:57 +0900

    メッセージ表示の不具合を修正

--- static/js/alert-message.js
+++ static/js/alert-message.js
@@ -1,6 +1,4 @@
-// 送信時にメッセージのチェックを行う
-document.messageForm.btn.addEventListener('click', function() {
-
+function alertMessage(){
   // メッセージ内容を取得
   let message = document.getElementById("message").value;
   // アラートフラグ
@@ -14,21 +12,15 @@ document.messageForm.btn.addEventListener('click', function() {
       break;
     }
   }
+  // 不適切なワードが入っていた場合
   if(alertFlg) {
-    var result = confirm('相手を傷つける言葉が含まれている可能性があります。本当に送信しますか？');
-    // "OK"/"キャンセル"により分岐
-    if( result ) {
-      // "OK"を選択したとき
-      doucument.messageForm.submit();
-    } else {
-      // "キャンセル"を選択したとき
-      document.getElementById("message").value = "";
-      window.location.reload();
-    }
+    alert('不適切なワードが入っています。投稿内容を確認してください');
+    return false;
+
+  // 上記以外の場合
   } else {
     doucument.messageForm.submit();
   }
-  
-})
+}
 
 const badMessages = ["バカ", "クソ", "死ね", "〇ね", "馬鹿", "はげ", "禿", "ハゲ", "〇げ"]
